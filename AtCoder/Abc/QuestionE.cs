5ccf40c084743d93117ceac362c5f982e063b8a5
Escaity <sfz2306@gmail.com>
2021-08-22 17:53:16 +0900

    Changed QuestionX.cs Contents

--- AtCoder/Abc/QuestionE.cs
+++ AtCoder/Abc/QuestionE.cs
@@ -8,29 +8,19 @@ namespace AtCoder.Abc
 {
     class QuestionE
     {
+        static System.IO.StreamWriter sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+
         public static void Main(string[] args)
         {
-            var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
             Console.SetOut(sw);
 
-            // 文字列の入力
-            string s = Console.ReadLine();
-
-            // 整数の入力
-            long n = long.Parse(Console.ReadLine());
-
-            // 文字列配列の入力
-            string[] inputStrArray = Console.ReadLine().Split(' ');
-
-            // 整数配列の入力
-            var inputLongArray = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+            string S = Console.ReadLine(); // 文字列の入力
+            long nl = long.Parse(Console.ReadLine()); // 整数の入力
+            string[] arrStr = Console.ReadLine().Split(' '); // 文字列配列の入力
+            var arrLong = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray(); // 整数配列の入力
 
 
-
-
-            string result = "";
-
-            Console.WriteLine(result);
+            Console.WriteLine();
 
             Console.Out.Flush();
         }

7cd5522dcc51055b22e8694a7d55595856b3b410
Escaity <sfz2306@gmail.com>
2021-09-05 23:22:10 +0900

    標準入力の変更

--- AtCoder/Abc/QuestionA.cs
+++ AtCoder/Abc/QuestionA.cs
@@ -9,18 +9,28 @@ namespace AtCoder.Abc
     public class QuestionA
     {
         static System.IO.StreamWriter sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
-
         public static void Main(string[] args)
         {
             Console.SetOut(sw);
+            string S = Console.ReadLine();
+            var N = long.Parse(Console.ReadLine()); // 整数の入力
+            var aS = Console.ReadLine().Split(' ').ToArray(); // 文字列配列の入力
+            var aN = Console.ReadLine().Split(' ').Select(x => long.Parse(x)).ToArray(); // 整数配列の入力
 
-            string S = Console.ReadLine(); // 文字列の入力
-            long nl = long.Parse(Console.ReadLine()); // 整数の入力
-            string[] arrStr = Console.ReadLine().Split(' '); // 文字列配列の入力
-            var arrLong = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray(); // 整数配列の入力
 
-
-            Console.WriteLine();
+            //二次元整数配列用
+            /*
+            var LL = new List<List<long>>();
+            for (int i = 0; i < (int)N; i++)
+			{
+                LL.Add(new List<long>());
+                LL[i].AddRange(Console.ReadLine().Split(' ').Select(x => long.Parse(x)).ToList());
+			}
+            foreach (var item in LL)
+	        {
+                Console.WriteLine(string.Join(" ",item));
+	        }
+            */
 
             Console.Out.Flush();
         }
